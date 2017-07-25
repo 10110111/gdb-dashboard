b929031876c8b67e384775d966569b7ca1555aef
Ruslan Kabatsayev <b7.10110111@gmail.com>
2017-07-25 11:29:33 +0300

    Handle exceptions in outer function, not inner ones

--- init.my/dashboard-setup.py
+++ init.my/dashboard-setup.py
@@ -12,33 +12,30 @@ class x86gpr(Dashboard.Module):
         return ansi(name,R.style_low)+' '+ansi(value,R.style_selected_1 if changed else '')
 
     def linesGPR(self,termWidth,styleChanged):
-        try:
-            if self.bits==32:
-                regNames=["EAX","ECX","EDX","EBX","ESP","EBP","ESI","EDI"]
-                regValues=run('printf "%08x,%08x,%08x,%08x,%08x,%08x,%08x,%08x",'+
-                              '$eax,$ecx,$edx,$ebx,$esp,$ebp,$esi,$edi').split(',')
-                if len(regValues)!=8:
-                    raise Exception("Registers unavailable")
-            else:
-                regNames=["RAX","RCX","RDX","RBX","RSP","RBP","RSI","RDI",
-                          "R8 ","R9 ","R10","R11","R12","R13","R14","R15"]
-                regValues=run('printf "%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,'+
-                              '%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx",'+
-                              '$rax,$rcx,$rdx,$rbx,$rsp,$rbp,$rsi,$rdi,'+
-                              '$r8,$r9,$r10,$r11,$r12,$r13,$r14,$r15').split(',')
-                if len(regValues)!=16:
-                    raise Exception("Registers unavailable")
+        if self.bits==32:
+            regNames=["EAX","ECX","EDX","EBX","ESP","EBP","ESI","EDI"]
+            regValues=run('printf "%08x,%08x,%08x,%08x,%08x,%08x,%08x,%08x",'+
+                          '$eax,$ecx,$edx,$ebx,$esp,$ebp,$esi,$edi').split(',')
+            if len(regValues)!=8:
+                raise Exception("Registers unavailable")
+        else:
+            regNames=["RAX","RCX","RDX","RBX","RSP","RBP","RSI","RDI",
+                      "R8 ","R9 ","R10","R11","R12","R13","R14","R15"]
+            regValues=run('printf "%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,'+
+                          '%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx,%016lx",'+
+                          '$rax,$rcx,$rdx,$rbx,$rsp,$rbp,$rsi,$rdi,'+
+                          '$r8,$r9,$r10,$r11,$r12,$r13,$r14,$r15').split(',')
+            if len(regValues)!=16:
+                raise Exception("Registers unavailable")
 
-            regs=dict(zip(regNames,regValues))
-            registers=[]
-            for name in regNames:
-                value=regs[name]
-                changed=self.table and self.table.get(name,'')!=value
-                self.table[name]=value
-                registers.append(self.formatReg(name,value,changed))
-            return registers
-        except Exception,e:
-            return [str(e)]
+        regs=dict(zip(regNames,regValues))
+        registers=[]
+        for name in regNames:
+            value=regs[name]
+            changed=self.table and self.table.get(name,'')!=value
+            self.table[name]=value
+            registers.append(self.formatReg(name,value,changed))
+        return registers
 
     def linesPC(self,termWidth,styleChanged):
         if self.bits==32:
@@ -53,4 +50,7 @@ class x86gpr(Dashboard.Module):
 
     def lines(self,termWidth,styleChanged):
         self.bits=32
-        return self.linesGPR(termWidth,styleChanged)+['']+self.linesPC(termWidth,styleChanged)
+        try:
+            return self.linesGPR(termWidth,styleChanged)+['']+self.linesPC(termWidth,styleChanged)
+        except Exception,e:
+            return [str(e)]
