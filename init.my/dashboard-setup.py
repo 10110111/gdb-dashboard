9b2555b3b8940750e987739ddb8d61c9daca7ed7
Ruslan Kabatsayev <b7.10110111@gmail.com>
2017-08-02 11:33:02 +0300

    Give knownRegs more x86-specific name

--- init.my/dashboard-setup.py
+++ init.my/dashboard-setup.py
@@ -1,7 +1,7 @@
 class x86regs(Dashboard.Module):
     "x86 registers view"
 
-    knownRegs=[
+    knownRegsX86=[
                 "eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi",
                 "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi",
                 "r8","r9","r10","r11","r12","r13","r14","r15",
@@ -497,7 +497,7 @@ class x86regs(Dashboard.Module):
             for string in infoAllRegs:
                 if re.match("^[a-zA-Z0-9]+ .*",string):
                     reg=string.split(' ')[0]
-                    if reg not in self.knownRegs:
+                    if reg not in self.knownRegsX86:
                         self.unknownRegs.append(reg)
         if len(self.unknownRegs)>0:
             print "WARNING: the following registers are not supported by this viewer:",unknownRegs
